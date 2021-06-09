336ee31ded6bc365fc8b7c01c39f1f347e19e49a
Muskan <muskanchhatrasal@gmail.com>
2021-06-09 19:57:28 +0530

    Color Generator Project - Color values and error

--- react-projects-master/09-color-generator/setup/src/App.js
+++ react-projects-master/09-color-generator/setup/src/App.js
@@ -10,13 +10,21 @@ function App() {
 
   const handleSubmit = (e)=>{
     e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      // console.log(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+    
   }
   return (
   <>
   <section className='container'>
     <h3>color generator</h3>
     <form onSubmit={handleSubmit}>
-     <input type='text' value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025'></input>
+     <input type='text' value={color} onChange={(e)=>setColor(e.target.value)} placeholder='#f15025' className={`${error ? 'error' : null}`}/>
      <button type='submit' className='btn'>submit</button>
     </form>
   </section>
