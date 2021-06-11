c57ae426a5da4abc55f4365780fb65e0869ef4ac
Muskan <muskanchhatrasal@gmail.com>
2021-06-11 19:11:15 +0530

    Color Generator - Copy To Clipboard (Completed Project)

--- react-projects-master/09-color-generator/setup/src/App.js
+++ react-projects-master/09-color-generator/setup/src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color,setColor] = useState('');
   const [error,setError] = useState(false);
-  const [list,setList] = useState([]);
+  const [list,setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e)=>{
     e.preventDefault();
@@ -32,7 +32,6 @@ function App() {
   </section>
   <section className='colors'>
     {list.map((color,index)=>{
-      console.log(color)
       return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
     })}
   </section>
