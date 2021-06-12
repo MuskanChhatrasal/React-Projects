6c72567f870f0cbe117def42a6f478329e116c7d
Muskan <muskanchhatrasal@gmail.com>
2021-06-12 19:50:48 +0530

    Grocery Bud - Add Items

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -11,7 +11,15 @@ function App() {
 
   const handleSubmit = (e) =>{
     e.preventDefault()
-    console.log('hello')
+    if(!name){
+      //display alert
+    }else if(name && isEditing){
+      //deal with edit
+    }else{
+      const newItem = {id:new Date().getTime().toString(),title:name};
+      setList([...list,newItem])
+      setName('')
+    }
   }
   return (
   <section className='section-center'>
@@ -25,10 +33,12 @@ function App() {
        </button>
     </div>
   </form>
-    <div className='grocery-container'>
-      <List />
+   {list.length > 0 && (
+     <div className='grocery-container'>
+      <List items={list}/>
       <button className='clear-btn'>clear items</button>
     </div>
+   )}
   </section>
   )
 }
