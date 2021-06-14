45313aee8927cc3f7dc26e9cbce1b5bdfc2cb0f5
Muskan <muskanchhatrasal@gmail.com>
2021-06-14 19:13:49 +0530

    Grocery Bud Project - Edit Item

--- react-projects-master/10-grocery-bud/setup/src/App.js
+++ react-projects-master/10-grocery-bud/setup/src/App.js
@@ -15,7 +15,16 @@ function App() {
       //display alert
       showAlert(true,'please enter value','danger')
     }else if(name && isEditing){
-      //deal with edit
+      setList(list.map((item)=>{
+        if(item.id === editID){
+          return {...item,title:name}
+        }
+        return item
+      }))
+      setName('');
+      setEditId(null);
+      setIsEditing(false);
+      showAlert(true,'value changed','success')
     }else{
       showAlert(true,'item added to the list','success')
       const newItem = {id:new Date().getTime().toString(),title:name};
@@ -34,6 +43,12 @@ function App() {
     showAlert(true,'item removed','danger');
     setList(list.filter((item)=>item.id !== id))
   }
+  const editItem = (id)=>{
+    const specificItem = list.find((item)=>item.id===id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  }
   return (
   <section className='section-center'>
   <form className='grocery-form' onSubmit={handleSubmit}>
@@ -48,7 +63,7 @@ function App() {
   </form>
    {list.length > 0 && (
      <div className='grocery-container'>
-      <List items={list} removeItem={removeItem}/>
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className='clear-btn' onClick={clearList}>clear items</button>
     </div>
    )}
