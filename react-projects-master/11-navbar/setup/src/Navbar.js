b66be7cd50e1d870165994a1eb834df2d54b7db7
Muskan <muskanchhatrasal@gmail.com>
2021-06-16 19:51:53 +0530

    Navbar Project - Simple Toggle

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -4,15 +4,17 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks,setShowLinks] = useState(false)
   return (
   <nav>
    <div className='nav-center'>
      <div className='nav-header'>
         <img src={logo} alt="logo" />
-        <button className='nav-toggle'>
+        <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
           <FaBars />
         </button>
      </div>
+     {showLinks && 
      <div className='links-container show-container'>
        <ul className='links'>
          {links.map((link)=>{
@@ -25,9 +27,10 @@ const Navbar = () => {
          })}
        </ul>
      </div>
+     }
      <ul className='social-icons'>
       {social.map((socialIcon)=>{
-        const {id,url,icon}=socialIcon
+        const {id,url,icon} = socialIcon
         return (
           <li key={id}>
             <a href={url}>{icon}</a>
