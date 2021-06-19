b6ecccde2c46750f908010e738aab9aa4d198ed7
Muskan <muskanchhatrasal@gmail.com>
2021-06-19 18:57:27 +0530

    Navbar Project - useRef (Completed)

--- react-projects-master/11-navbar/setup/src/Navbar.js
+++ react-projects-master/11-navbar/setup/src/Navbar.js
@@ -5,6 +5,16 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks,setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if(showLinks){
+      linksContainerRef.current.style.height=`${linksHeight}px`
+    }else{
+      linksContainerRef.current.style.height='0px'
+    }
+  },[showLinks])
   return (
   <nav>
    <div className='nav-center'>
@@ -15,8 +25,8 @@ const Navbar = () => {
         </button>
      </div>
       
-     <div className={`${showLinks?'links-container show-container':'links-container'}`}>
-       <ul className='links'>
+     <div className='links-container' ref={linksContainerRef}>
+       <ul className='links' ref={linksRef}>
          {links.map((link)=>{
            const {id,url,text} = link
            return (
