ad2d02c6057eda6b782cd354749819ae8d33e739
Muskan <muskanchhatrasal@gmail.com>
2021-07-06 19:31:43 +0530

    Sidebar/Modal Project - Structure

--- react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
+++ react-projects-master/12-sidebar-modal/setup/src/Sidebar.js
@@ -4,7 +4,41 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+  <aside className={`sidebar`}>
+    <div className='sidebar-header'>
+      <img src={logo} className='logo' alt='coding addict' />
+      <button className='close-btn'>
+        <FaTimes />
+      </button>
+    </div>
+    <ul className='links'>
+      {links.map((link)=>{
+        const {id,icon,text,url}=link;
+        return(
+          <li key={id}>
+            <a href={url}>
+              {icon}
+              {text}
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+    <ul className='social-icons'>
+      {social.map((link)=>{
+        const {id,url,icon}=link;
+        return(
+          <li key={id}>
+            <a href={url}>
+              {icon}
+            </a>
+          </li>
+        )
+      })}
+    </ul>
+  </aside>
+  )
 }
 
 export default Sidebar
